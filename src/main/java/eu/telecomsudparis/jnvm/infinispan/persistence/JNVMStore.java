3696e8ecc211bc2dd2d1153c72b0e23379510a07
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-02-24 15:17:27 +0000

    JNVMStore: SegmentedAdvancedLoadWriteStore implementation
        Note that altough a Segmented interface is implemented, the store
        itself is not yet segmented and actually ignores the segment integer
        parameter.

--- src/main/java/eu/telecomsudparis/jnvm/infinispan/persistence/JNVMStore.java
+++ src/main/java/eu/telecomsudparis/jnvm/infinispan/persistence/JNVMStore.java
@@ -4,9 +4,10 @@ import java.util.concurrent.Executor;
 
 import org.infinispan.commons.configuration.ConfiguredBy;
 import org.infinispan.commons.persistence.Store;
+import org.infinispan.commons.util.IntSet;
 import org.infinispan.persistence.spi.MarshallableEntry;
 import org.infinispan.persistence.spi.MarshallableEntryFactory;
-import org.infinispan.persistence.spi.AdvancedLoadWriteStore;
+import org.infinispan.persistence.spi.SegmentedAdvancedLoadWriteStore;
 import org.infinispan.persistence.spi.InitializationContext;
 import org.kohsuke.MetaInfServices;
 import java.util.function.Predicate;
@@ -19,7 +20,7 @@ import eu.telecomsudparis.jnvm.offheap.OffHeapObject;
 @Store
 @MetaInfServices
 @ConfiguredBy(JNVMStoreConfiguration.class)
-public class JNVMStore<K extends OffHeapObject, V extends OffHeapObject> implements AdvancedLoadWriteStore<K, V> {
+public class JNVMStore<K extends OffHeapObject, V extends OffHeapObject> implements SegmentedAdvancedLoadWriteStore<K, V> {
 
     private RecoverableStrongHashMap<K, V> backend = null;
 
@@ -28,7 +29,22 @@ public class JNVMStore<K extends OffHeapObject, V extends OffHeapObject> impleme
     private JNVMStoreConfiguration configuration;
 
     @Override
-    public boolean contains(Object o) {
+    public boolean contains(Object o) { return contains(0, o); }
+    @Override
+    public MarshallableEntry<K, V> loadEntry(Object o) { return get(0, o); }
+    @Override
+    public boolean delete(Object o) { return delete(0, o); }
+    @Override
+    public void write(MarshallableEntry<? extends K, ? extends V> marshallableEntry) { write(0, marshallableEntry); }
+    @Override
+    public Publisher<MarshallableEntry<K, V>> entryPublisher(Predicate<? super K> filter, boolean fetchValue, boolean fetchMetadata) { throw new UnsupportedOperationException("entryPublisher no segments"); }
+    @Override
+    public int size() { return backend.size(); }
+    @Override
+    public void clear() { backend.clear(); }
+
+    @Override
+    public boolean contains(int segment, Object o) {
         return backend.containsKey(o);
     }
 
@@ -40,7 +56,7 @@ public class JNVMStore<K extends OffHeapObject, V extends OffHeapObject> impleme
     }
 
     @Override
-    public MarshallableEntry<K, V> loadEntry(Object o) {
+    public MarshallableEntry<K, V> get(int segment, Object o) {
         return entryFactory.create(o, backend.get(o));
     }
 
@@ -57,22 +73,22 @@ public class JNVMStore<K extends OffHeapObject, V extends OffHeapObject> impleme
     }
 
     @Override
-    public boolean delete(Object o) {
+    public boolean delete(int segment, Object o) {
         return backend.remove(o) != null;
     }
 
     @Override
-    public void write(MarshallableEntry<? extends K, ? extends V> marshallableEntry) {
+    public void write(int segment, MarshallableEntry<? extends K, ? extends V> marshallableEntry) {
         backend.put(marshallableEntry.getKey(), marshallableEntry.getValue());
     }
 
     @Override
-    public int size() {
+    public int size(IntSet segments) {
         return backend.size();
     }
 
     @Override
-    public void clear() {
+    public void clear(IntSet segments) {
         backend.clear();
     }
 
@@ -81,11 +97,16 @@ public class JNVMStore<K extends OffHeapObject, V extends OffHeapObject> impleme
             Executor arg0,
             org.infinispan.persistence.spi.AdvancedCacheWriter.PurgeListener<? super K> arg1) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
-    public Publisher<MarshallableEntry<K, V>> entryPublisher(Predicate<? super K> filter,
+    public Publisher<K> publishKeys(IntSet segments, Predicate<? super K> filter) {
+        throw new UnsupportedOperationException("publishKeys");
+    }
+
+    @Override
+    public Publisher<MarshallableEntry<K, V>> entryPublisher(IntSet segments,
+                                                             Predicate<? super K> filter,
                                                              boolean fetchValue,
                                                              boolean fetchMetadata) {
         throw new UnsupportedOperationException("entryPublisher");
